2929f6b9469c1258127ba9ed523fbc233e27a425
Soren Lassen <slassen@csumb.edu>
2016-10-17 23:11:59 -0700

    Mock ajax call with jasmine-ajax.
    Commented out jasmine-jquery. I saw somewhere that it could clash with jasmine-ajax.
    
    Test plan: mvn jasmine:test

--- src/test/webapp/js/rest_spec.js
+++ src/test/webapp/js/rest_spec.js
@@ -2,35 +2,24 @@
 // the jetty webapp which is running during the integration-test phase.
 BaseURL = "http://localhost:8080";
 
-/*
-describe('REST match call', function() {
-  var newPattern;
-  var failure = "none";
-  beforeEach(function(done) {
-    spyOn($, 'get');
-    rest_match(
-      { oldPattern: ".a.", oldGuesses: "ab", newGuesses: "c" },
-      function(pattern) { newPattern = pattern; done(); },
-      function(fail) { failure = fail; done(); }
-    );
-  });
-  it('calls correct URL', function(done) {
-    expect(newPattern).toEqual("ca.");
-    expect(failure).toEqual("none");
-    done();
-  });
-});
-*/
-
+// See: http://jasmine.github.io/2.0/ajax.html
 describe('REST start call', function() {
-  var startPattern;
-  beforeEach(function(done) {
-    spyOn($, 'get').and.callThrough();
-    rest_start(
-      function(pattern) { startPattern = pattern; done(); }
-    );
+  beforeEach(function() {
+    jasmine.Ajax.install();
+  });
+  afterEach(function() {
+    jasmine.Ajax.uninstall();
   });
   it('calls correct URL', function() {
-    expect(startPattern).toEqual("...");
+    var onSuccess = jasmine.createSpy("success");
+    rest_start(onSuccess);
+    expect(jasmine.Ajax.requests.mostRecent().url).toBe(BaseURL + '/rest/start');
+    expect(onSuccess).not.toHaveBeenCalled();
+    jasmine.Ajax.requests.mostRecent().respondWith({
+      "status": 200,
+      "contentType": 'text/plain',
+      "responseText": '...'
+    });
+    expect(onSuccess).toHaveBeenCalled();
   });
 });
